bd8b8c69c9389c56e8b7fa2f238a1ec94c26b386
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2026-02-04 17:37:08 +0500

    feat[frontend]: Add url port to .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = Number(env.VITE_FRONTEND_PORT) || 5173
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    server: {
+      port
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
 })
